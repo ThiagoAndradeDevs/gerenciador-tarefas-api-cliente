d5164c91cf970d446ec5eaa3a15126bfc5310236
Thiago <thiago.andrade.devs@gmail.com>
2021-12-16 15:20:59 -0300

    axios

--- src/atualizar/atualizar-tarefas.js
+++ src/atualizar/atualizar-tarefas.js
@@ -3,21 +3,28 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Form, Modal, Jumbotron } from 'react-bootstrap';
 import { navigate, A } from 'hookrouter';
+import axios from 'axios';
+
 
 function AtualizarTarefa(props) {
+  const API_URL_TAREFAS = 'http://localhost:3001/gerenciador-tarefas/';
   const [tarefa, setTarefa] = useState(' ')
   const [exibirModal, setExibirModal] = useState(false);
   const [formValidado, setFormValidado] = useState(false)
   const [carregarTarefa, setCarregarTarefa] = useState(true);
   const [exibirModalErro, setExibirModalErro] = useState(false);
   useEffect(() => {
+    async function obterTarefa() {
+      try {
+        let { data } = await axios.get(API_URL_TAREFAS + props.id);
+        setTarefa(data.nome);
+      } catch (err) {
+        navigate('/');
+      }
+    }
+
     if (carregarTarefa) {
-      const tarefasDb = localStorage['tarefas'];
-      const tarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
-      const tarefa = tarefas.filter(
-        t => t.id === parseInt(props.id)
-      )[0];
-      setTarefa(tarefa.nome);
+      obterTarefa();
       setCarregarTarefa(false);
     }
   }, [carregarTarefa, props])
