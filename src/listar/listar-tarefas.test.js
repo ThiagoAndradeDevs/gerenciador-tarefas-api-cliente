8a7ff2d4a287ddebd38157ddb2409d97a8352a7e
Thiago <thiago.andrade.devs@gmail.com>
2021-12-16 16:33:02 -0300

    test

--- src/listar/listar-tarefas.test.js
+++ src/listar/listar-tarefas.test.js
@@ -12,20 +12,19 @@ describe.skip('Teste do componente de listagem de tarefas', () => {
   const nomePrimeiraTarefa = 'Primeira tarefa';
   const nomeSegundaTarefa = 'Segunda tarefa';
   const nomeTerceiraTarefa = 'Terceira tarefa';
+  const listaTarefas = {
+    totalItens: 3,
+    tarefas: [
+      new Tarefa(1, nomePrimeiraTarefa, false),
+      new Tarefa(2, nomeSegundaTarefa, false),
+      new Tarefa(3, nomeTerceiraTarefa, false)
+    ],
+    pagina: 1
+  };
 
 })
 it('deve exibir uma tabela contendo 3 tarefas', async () => {
-  axiosMock.get.mockResolvedValueOnce({
-    data: {
-      totalItens: 3,
-      tarefas: [
-        new Tarefa(1, nomePrimeiraTarefa, false),
-        new Tarefa(2, nomeSegundaTarefa, false),
-        new Tarefa(3, nomeTerceiraTarefa, false)
-      ],
-      pagina: 1
-    }
-  })
+  axiosMock.get.mockResolvedValueOnce({ data: listaTarefas });
 
   const { findByTestId } = render(<ListarTarefas />);
   const tabela = await findByTestId('tabela');
@@ -33,10 +32,18 @@ it('deve exibir uma tabela contendo 3 tarefas', async () => {
   expect(tabela).toHaveTextContent(nomeSegundaTarefa);
   expect(tabela).toHaveTextContent(nomeTerceiraTarefa);
 })
-it.skip('deve filtrar os dados da tabela de tarefas', () => {
-  const { getByTestId } = render(<ListarTarefas />);
-  fireEvent.change(getByTestId('txt-tarefas'), { target: { value: nomePrimeiraTarefa } });
-  const tabela = getByTestId('tabela');
+it('deve filtrar os dados da tabela de tarefas', async () => {
+  axiosMock.get.mockResolvedValueOnce({ data: listaTarefas })
+  axiosMock.get.mockResolvedValueOnce({
+    data: {
+      totalItens: 1,
+      tarefas: [new Tarefa(1, nomePrimeiraTarefa, false)],
+      pagina: 1
+    }
+  })
+  const { findByTestId } = render(<ListarTarefas />);
+  fireEvent.change(await findByTestId('txt-tarefas'), { target: { value: nomePrimeiraTarefa } });
+  const tabela = await findByTestId('tabela');
   expect(tabela).toHaveTextContent(nomePrimeiraTarefa);
   expect(tabela).not.toHaveTextContent(nomeSegundaTarefa);
   expect(tabela).not.toHaveTextContent(nomeTerceiraTarefa);
