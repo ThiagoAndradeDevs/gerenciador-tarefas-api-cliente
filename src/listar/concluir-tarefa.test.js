d22d5468607263fa42cc336782431bbb60237e68
Thiago <thiago.andrade.devs@gmail.com>
2021-12-17 15:24:02 -0300

    test

--- src/listar/concluir-tarefa.test.js
+++ src/listar/concluir-tarefa.test.js
@@ -10,7 +10,7 @@ describe.skip('Teste do componente de conclusão de tarefas', () => {
   const nomeTarefa = 'Tarefa teste';
   const tarefa = new Tarefa(1, nomeTarefa, false);
 
-  it.skiped('deve reinderizar o componente sem erros', () => {
+  it('deve reinderizar o componente sem erros', () => {
     const div = document.createElement('div');
     ReactDOM.render(
       <ConcluirTarefa tarefa={tarefa} recarregarTarefas={() => false} />, div);
@@ -23,7 +23,7 @@ describe.skip('Teste do componente de conclusão de tarefas', () => {
     fireEvent.click(getByTestId('btn-abrir-modal'));
     expect(getByTestId('modal')).toHaveTextContent(nomeTarefa);
   })
-  it('deve concluir uma tarefa', () => {
+  it.skiped('deve concluir uma tarefa', () => {
     localStorage['tarefa'] = JSON.stringify([tarefa]);
     const { getByTestId } = render(
       <ConcluirTarefa tarefa={tarefa} recarregarTarefas={() => false} />
