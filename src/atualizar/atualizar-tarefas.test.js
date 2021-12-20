40e960bac54097d89877b238ca2a91c61f5a7022
Thiago <thiago.andrade.devs@gmail.com>
2021-12-20 16:23:50 -0300

    axios

--- src/atualizar/atualizar-tarefas.test.js
+++ src/atualizar/atualizar-tarefas.test.js
@@ -6,16 +6,17 @@ import AtualizarTarefa from './atualizar-tarefas';
 import Tarefa from "../models/tarefa.model";
 import { render, FireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { A } from 'hookrouter';
+import axiosMock from 'axios';
 
 
-describe.skiped('Teste do componente de atualizaçao de tarefas', () => {
+
+describe('Teste do componente de atualizaçao de tarefas', () => {
   const tarefaId = 1;
-  const tarefa = new Tarefa(tarefaId, 'Nova tarefa', false);
-
-  it('deve exibir a modal de sucesso ao atualizar uma tarefa', () => {
-    const { getByTestId } = render(<AtualizarTarefa id={tarefaId} />);
-    fireEvent.cliclk(getByTestId('btn-atualizar'));
-    expect(getByTestId('modal')).toHaveTextContent('Sucesso');
+  it('deve exibir a modal de sucesso ao atualizar uma tarefa', async () => {
+    axiosMock.get.mockResolvedValueOnce({ data: { nome: 'Estudar React' } })
+    const { findByTestId } = render(<AtualizarTarefa id={tarefaId} />);
+    fireEvent.cliclk(await findByTestId('btn-atualizar'));
+    const modal = await findByTestId('modal');
+    expect(modal).toHaveTextContent('Sucesso');
   });
 })
